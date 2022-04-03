GoogleCloudPlatform/cloud-foundation-fabric

GKE multitenant
Co-authored-by: Daniel Marzini <dmarzi@google.com>

fast/stages/01-resman/branch-networking.tf
@@ -65,7 +65,8 @@ module "branch-network-prod-folder" {
   name   = "prod"
   iam = {
     "roles/compute.xpnAdmin" = [
-      module.branch-teams-prod-projectfactory-sa.iam_email
+      module.branch-teams-prod-projectfactory-sa.iam_email,
+      module.branch-gke-multitenant-prod-sa.iam_email,
     ]
   }
 }
@@ -76,7 +77,8 @@ module "branch-network-dev-folder" {
   name   = "dev"
   iam = {
     "roles/compute.xpnAdmin" = [
-      module.branch-teams-dev-projectfactory-sa.iam_email
+      module.branch-teams-dev-projectfactory-sa.iam_email,
+      module.branch-gke-multitenant-dev-sa.iam_email,
     ]
   }
 }

trivelaapp/gcp-cloud-foundation-fabric

fast/stages/01-resman/branch-networking.tf
@@ -69,7 +69,8 @@ module "branch-network-prod-folder" {
   iam = {
     "roles/compute.xpnAdmin" = [
       module.branch-dp-prod-sa.iam_email,
-      module.branch-teams-prod-pf-sa.iam_email
+      module.branch-teams-prod-pf-sa.iam_email,
+      module.branch-gke-multitenant-prod-sa.iam_email,
     ]
   }
   tag_bindings = {
@@ -84,7 +85,8 @@ module "branch-network-dev-folder" {
   iam = {
     (local.custom_roles.service_project_network_admin) = [
       module.branch-dp-dev-sa.iam_email,
-      module.branch-teams-dev-pf-sa.iam_email
+      module.branch-teams-dev-pf-sa.iam_email,
+      module.branch-gke-multitenant-dev-sa.iam_email,
     ]
   }
   tag_bindings = {